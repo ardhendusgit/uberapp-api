de111347b549b05927a1cb301f0bb277a94c5c1a
Ardhendu Shekhar <crackednutxx@gmail.com>
2024-07-07 23:01:15 +0530

    Update comment backend.tf

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
 
   backend "s3" {
     bucket = "hard-way-state-bucket-ardhendu" # Mention your S3 bucket here to save your infrastructure's state
-    key    = "terraform.tfstate"
+    key    = "terraform.tfstate" # S3 bucket keys can be longer as well, example, state/terraform-state.tfstate. Choose accordingly.
     region = "us-east-2" 
   }
 }

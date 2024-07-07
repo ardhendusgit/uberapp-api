26a0d148f121e446c69f7886e323b949fc22bbce
Ardhendu Shekhar <crackednutxx@gmail.com>
2024-07-07 22:58:50 +0530

    Add comment to backend.tf

--- backend.tf
+++ backend.tf
@@ -6,8 +6,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "hard-way-state-bucket-ardhendu"
+    bucket = "hard-way-state-bucket-ardhendu" # Mention your S3 bucket here to save your infrastructure's state
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-2" 
   }
 }
